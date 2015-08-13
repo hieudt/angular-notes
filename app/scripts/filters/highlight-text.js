fc994e8e5c16ee25874238fc955aae9999fe1a17
Hieu Do <mr.hieudt@gmail.com>
2015-08-13 11:37:50 +0700

    Revise code for highlight-text filter

--- app/scripts/filters/highlight-text.js
+++ app/scripts/filters/highlight-text.js
@@ -11,10 +11,12 @@
 angular.module('angularNotesApp')
   .filter('highlightText', function ($sce) {
     return function (text, phrase) {
-      if (phrase) {
-        text = text.replace(new RegExp('('+phrase+')', 'gi'), '<span class="highlighted">$1</span>');
+      if (text) {
+        if (phrase) {
+          text = text.replace(new RegExp('(' + phrase + ')', 'gi'), '<span class="highlighted">$1</span>');
+        }
+        return $sce.trustAsHtml(text)
       }
-
-      return $sce.trustAsHtml(text)
+      return text;
     };
   });
